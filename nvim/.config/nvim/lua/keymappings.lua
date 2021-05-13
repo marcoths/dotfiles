707b2c335596776b22f2e794f5205b9715c69524
Marco Hernandez <marcohernandez@spatialos.com>
2021-05-13 20:17:17 +0100

    neovim keymappings

--- nvim/.config/nvim/lua/keymappings.lua
+++ nvim/.config/nvim/lua/keymappings.lua
@@ -3,3 +3,8 @@ local utils = require('utils')
 utils.map('n', '<C-l>', '<cmd>noh<CR>') -- Clear highlights
 utils.map('i', 'jk', '<Esc>')           -- jk to escape
 utils.map('n','<leader>cd',':cd %:p:h<CR>:pwd<CR>') -- change pwd to opened file
+utils.map('n','<Up>', ':resize +2<CR>')
+utils.map('n','<Down>', ':resize -2<CR>')
+utils.map('n','<Left>', ':vertical resize +2<CR>')
+utils.map('n','<Right>', ':vertical resize -2<CR>')
+
--- nvim/.config/nvim/lua/settings.lua
+++ nvim/.config/nvim/lua/settings.lua
@@ -15,12 +15,14 @@ utils.opt('o', 'ignorecase', true)
 utils.opt('o', 'scrolloff', 4)
 utils.opt('o', 'shiftround', true)
 utils.opt('o', 'smartcase', true)
+utils.opt('o', 'mouse', 'a')
 utils.opt('o', 'splitbelow', true)
 utils.opt('o', 'splitright', true)
 utils.opt('o', 'wildmode', 'list,full')
 utils.opt('w', 'relativenumber', true)
 utils.opt('w', 'number', true)
 utils.opt('o', 'clipboard', 'unnamed,unnamedplus')
+vim.env.RTP=vim.env.XDG_CONFIG_HOME.."/nvim"
 
 -- Highlight on yank
 vim.cmd 'au TextYankPost * lua vim.highlight.on_yank {on_visual = false}'
