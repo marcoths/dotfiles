c01355e8ae4e6c5be46eecc09f5b29a779dc39f9
Marco T Hernandez <marco.t.hernandez@jpmorgan.com>
2022-03-03 15:57:08 +0000

    added nvim-tree and changed colorscheme

--- nvim/lua/user/plugins.lua
+++ nvim/lua/user/plugins.lua
@@ -38,6 +38,9 @@ vim.cmd([[
   use "wbthomason/packer.nvim" -- Have packer manage itself
   use "nvim-lua/popup.nvim" -- An implementation of the Popup API from vim in Neovim
   use "nvim-lua/plenary.nvim" -- Useful lua functions used ny lots of plugins
+  use "lunarvim/darkplus.nvim"
+  use 'kyazdani42/nvim-web-devicons'
+  use 'kyazdani42/nvim-tree.lua'
   
   if PACKER_BOOTSTRAP then
     require("packer").sync()
