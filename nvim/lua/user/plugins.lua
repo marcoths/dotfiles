590fae1af0fc8abf597cb695322cc6c413a58255
Marco T Hernandez <marco.t.hernandez@jpmorgan.com>
2022-03-07 12:50:08 +0000

    added cmp and lsp settings

--- nvim/lua/user/plugins.lua
+++ nvim/lua/user/plugins.lua
@@ -41,7 +41,23 @@ vim.cmd([[
   use "lunarvim/darkplus.nvim"
   use 'kyazdani42/nvim-web-devicons'
   use 'kyazdani42/nvim-tree.lua'
-  
+  use "hrsh7th/cmp-nvim-lsp"
+    -- cmp plugins
+  use "hrsh7th/nvim-cmp" -- The completion plugin
+  use "hrsh7th/cmp-buffer" -- buffer completions
+  use "hrsh7th/cmp-path" -- path completions
+  use "hrsh7th/cmp-cmdline" -- cmdline completions
+  use "saadparwaiz1/cmp_luasnip" -- snippet completions
+
+  -- snippets
+  use "L3MON4D3/LuaSnip" --snippet engine
+  use "rafamadriz/friendly-snippets" -- a bunch of snippets to use
+
+    -- LSP
+  use "neovim/nvim-lspconfig" -- enable LSP
+  use "williamboman/nvim-lsp-installer" -- simple to use language server installer
+  use "tamago324/nlsp-settings.nvim" -- language server settings defined in json for
+
   if PACKER_BOOTSTRAP then
     require("packer").sync()
   end
