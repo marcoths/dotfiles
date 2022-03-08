11f0e0db6476653b7fbcc3c70173e030e083d43e
Marco T Hernandez <marco.t.hernandez@jpmorgan.com>
2022-03-08 09:12:27 +0000

    added telescope and treesitter

--- nvim/lua/user/plugins.lua
+++ nvim/lua/user/plugins.lua
@@ -58,6 +58,13 @@ vim.cmd([[
   use "williamboman/nvim-lsp-installer" -- simple to use language server installer
   use "tamago324/nlsp-settings.nvim" -- language server settings defined in json for
 
+  use "nvim-telescope/telescope.nvim"
+    -- Treesitter
+  use {
+    "nvim-treesitter/nvim-treesitter",
+    run = ":TSUpdate",
+  }
+
   if PACKER_BOOTSTRAP then
     require("packer").sync()
   end
